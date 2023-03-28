facc5f2e873920a87e0ecdaeae1a3b371418b78b
Joe Polny <joepolny@gmail.com>
2023-03-28 09:59:26 +0200

    seperate deploy file

--- template_content/app.py
+++ template_content/app.py
@@ -3,29 +3,14 @@ from pyteal import *
 
 app = Application("HelloWorld")
 
+class Methods:
+    @app.external
+    def hello(name: abi.String, *, output: abi.String) -> Expr:
+        return output.set(Concat(Bytes("Hello, "), name.get()))
 
-@app.external
-def hello(name: abi.String, *, output: abi.String) -> Expr:
-    return output.set(Concat(Bytes("Hello, "), name.get()))
+    @app.delete(authorize=Authorize.only(Global.creator_address()))
+    def delete() -> Expr:
+        return Approve()
 
-
-@app.delete(authorize=Authorize.only(Global.creator_address()))
-def delete() -> Expr:
-    return Approve()
-
-
-app.build().export("./artifacts")
-
-accounts = sandbox.kmd.get_accounts()
-sender = accounts[0]
-
-app_client = client.ApplicationClient(
-    client=sandbox.get_algod_client(),
-    app=app,
-    sender=sender.address,
-    signer=sender.signer,
-)
-
-app_client.create()
-return_value = app_client.call(hello, name="Beaker").return_value
-print(return_value)
+if __name__ == "__main__":
+    app.build().export("./artifacts")
