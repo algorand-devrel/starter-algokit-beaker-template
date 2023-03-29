ed33b94eda72061fcdfa1ae7d55b6a3afb43c5fc
Joe Polny <joepolny@gmail.com>
2023-03-29 12:56:24 -0400

    address mypy self errors

--- template_content/app.py
+++ template_content/app.py
@@ -3,14 +3,13 @@ from pyteal import *
 
 app = Application("HelloWorld")
 
-class Methods:
-    @app.external
-    def hello(name: abi.String, *, output: abi.String) -> Expr:
-        return output.set(Concat(Bytes("Hello, "), name.get()))
+@app.external
+def hello(name: abi.String, *, output: abi.String) -> Expr:
+    return output.set(Concat(Bytes("Hello, "), name.get()))
 
-    @app.delete(bare=True, authorize=Authorize.only(Global.creator_address()))
-    def delete() -> Expr:
-        return Approve()
+@app.delete(bare=True, authorize=Authorize.only(Global.creator_address()))
+def delete() -> Expr:
+    return Approve()
 
 if __name__ == "__main__":
     app.build().export("./artifacts")
